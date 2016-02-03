735be2a1c60c28970e067c50ed94b8c3661f74ce
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2016-02-03 14:55:43 +0100

    show more functionality

--- js/news_app.js
+++ js/news_app.js
@@ -1,3 +1,14 @@
 $(function (){
-	
+	$("#comments").find("li:lt(3)").show();
+	$(document).on("click", "#showMoreComments", function(){
+		$("#comments").find("li").show();
+		$(this).text("hide");
+		$(this).attr("id", "hideComments");
+	});
+	$(document).on("click", "#hideComments", function(){
+		$("#comments").find("li").hide();
+		$("#comments").find("li:lt(3)").show();
+		$(this).text("show more");
+		$(this).attr("id", "showMoreComments");
+	});
 });
